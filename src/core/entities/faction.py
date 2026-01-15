9f98b2301a7ca6822405607428d651c715159c09
thetrueryan <georgytoper@gmail.com>
2026-01-15 14:03:31 +0400

    udate faction methods, to make more old style copmlexity and faster

--- src/core/entities/faction.py
+++ src/core/entities/faction.py
@@ -36,37 +36,37 @@ class Faction:
     base: Base
     is_ai: bool
     alive: bool = True
-    buildings: list[Building]
+    buildings: dict[BuildingType, set[Building]]
 
     @property
     def all_buildings(self) -> set[Building]:
-        """Get all building (excluding base, as it's stored separately)."""
-        return [building.coord for building in self.buildings]
+        """Get all building"""
+        return {building for building in self.buildings.values()}
 
     @property
     def territory(self) -> set[Territory]:
         """Get territory (returns reference to actual set)."""
-        return [building.coord for building in self.buildings if building.type == BuildingType.TERRITORY]
+        return self.buildings[BuildingType.TERRITORY]
     
     @property
     def bridges(self) -> set[Bridge]:
         """Get bridge (returns reference to actual set)."""
-        return [building.coord for building in self.buildings if building.type == BuildingType.BRIDGE]
+        return self.buildings[BuildingType.BRIDGE]
 
     @property
     def towers(self) -> set[Tower]:
         """Get tower (returns reference to actual set)."""
-        return [building.coord for building in self.buildings if building.type == BuildingType.TOWER]
+        return self.buildings[BuildingType.TOWER]
 
     @property
     def portals(self) -> set[Portal]:
         """Get portal (returns reference to actual set)."""
-        return [building.coord for building in self.buildings if building.type == BuildingType.PORTAL]
+        return self.buildings[BuildingType.PORTAL]
 
     @property
     def fortresses(self) -> set[Fortress]:
         """Get fortress (returns reference to actual set)."""
-        return [building.coord for building in self.buildings if building.type == BuildingType.FORTRESS]
+        return self.buildings[BuildingType.FORTRESS]
 
     @property
     def score(self) -> int:
@@ -82,32 +82,31 @@ class Faction:
         """Revive faction."""
         self.alive = True
 
-    def get_building(self, coord: Coord) -> Building | None:
-        for b in self.buildings:
-            if b.coord == coord:
-                return b
+    def get_building(self, building_type: BuildingType, coord: Coord) -> Building | None:
+        if building_type in self.buildings:
+            buildings = self.buildings[building_type]
+            for b in buildings:
+                if b.coord == coord and b.type == building_type:
+                    return b
+            return None
         return None
 
     def add_building(self, building: Building) -> bool:
         if self.base and building.type == BuildingType.BASE:
             return False
-        self.buildings.append(building)
+        
+        if building.type not in self.buildings:
+            self.buildings[building.type] = set()
+        self.buildings[building.type].add(building)
         return True
 
-    def remove_building(self, coord: Coord) -> bool:
-        for idx, b in enumerate(self.buildings):
-            if b.coord == coord:
-               removed_building = self.buildings.pop(idx)
-               if removed_building:
-                   return True
-        return False           
-    
-    def owns(self, coord: Coord) -> bool:
-        """Check if faction owns given coordinate."""
-        if self.base.coord == coord:
-            return True
-        else:
-            for b in self.buildings:
+    def remove_building(self, building_type: BuildingType, coord: Coord) -> bool:
+        if building_type in self.buildings:
+            buildings = self.buildings[building_type]
+            for b in buildings:
                 if b.coord == coord:
+                    buildings.discard(b)
                     return True
-        return False
+            return False
+        return False           
+        
